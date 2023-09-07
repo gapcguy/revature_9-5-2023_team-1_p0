2941dc5156bcd0e32582076e1915f6e2c6becbfd
akashkumar1691 <akashkumar1691@berkeley.edu>
2023-09-07 11:45:23 -0700

    update toyservice

--- team1-p0-api/src/main/java/Service/ToyService.java
+++ team1-p0-api/src/main/java/Service/ToyService.java
@@ -1,4 +1,19 @@
 package Service;
 
+import DAO.ToyDAO;
+import Model.Toy;
+
+import java.util.List;
+
 public class ToyService {
+
+    ToyDAO toyDAO;
+
+    public ToyService(){
+        this.toyDAO = new ToyDAO();
+    }
+    public List<Toy> getAvailableToys(){
+        return toyDAO.getAvailableToys();
+    }
+
 }
